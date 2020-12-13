276ccde9264a8ab1080c39f73a4ae646fc252723
chili <1032569304@qq.com>
2020-12-13 21:13:53 +0800

    配置 webpack 使用 loader

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,17 +1,27 @@
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 import React from "react";
-import x from 'icons/label.svg'
-console.log(x)
+require ('icons/label.svg')
+require('icons/money.svg')
+require('icons/chart.svg')
 
 const NavWrapper = styled.nav`
-  border:1px solid red;
+  line-height: 24px;
+  box-shadow: 0 0 3px rgba(0,0,0,0.25);
   >ul{
     display: flex;
   >li{
     width: 33.333333%;
     text-align: center;
-    padding: 16px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 4px 0;
+    .icon{
+    width: 24px;
+    height: 24px;
+    }
   }
 }
 `
@@ -21,13 +31,21 @@ const Nav = () =>{
     <NavWrapper>
       <ul>
         <li>
-          <img src={x} alt=""/>
+          <svg className="icon">
+            <use xlinkHref='#label'/>
+          </svg>
           <Link to="/tags">标签</Link>
         </li>
         <li>
+          <svg  className="icon">
+            <use xlinkHref='#money'/>
+          </svg>
           <Link to="/money">记账</Link>
         </li>
         <li>
+          <svg  className="icon">
+            <use xlinkHref='#chart'/>
+          </svg>
           <Link to="/statistics">统计</Link>
         </li>
       </ul>
