f26f1d23a546528ae3bbb297718deada046e8af3
chili <1032569304@qq.com>
2020-12-13 11:14:11 +0800

    添加 React-Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,78 @@
 import React from "react";
-import "./App.css";
-
-
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect}
+  from "react-router-dom";
 function App() {
   return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/tags">标签页</Link>
+          </li>
+          <li>
+            <Link to="/money">记账</Link>
+          </li>
+          <li>
+            <Link to="/statistics">统计</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Switch>
+          <Redirect exact from="/" to="/money"/>
+          <Route exact path="/tags">
+            <Tags />
+          </Route>
+          <Route path="/money">
+            <Money />
+          </Route>
+          <Route path="/statistics">
+            <Statistics/>
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Tags() {
+  return (
     <div>
-      ...
+      <h2>Home</h2>
     </div>
   );
 }
 
+function Money() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Statistics() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
+
+function NoMatch(){
+  return (
+    <div>
+      页面找不到啦
+    </div>
+  )
+}
 export default App;
