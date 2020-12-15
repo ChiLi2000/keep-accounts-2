44477c8963ee6a2726f3e02cd47f092ef2a102ad
chili <1032569304@qq.com>
2020-12-15 16:56:33 +0800

    Money.tsx UI 完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -5,6 +5,11 @@ import styled from "styled-components";
 const TagsSection = styled.section`
   background: #FFFFFF;
   padding: 12px 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
   > ol{margin:0 -12px;
     >li{
     background: #D9D9D9;
@@ -123,9 +128,13 @@ const NumberPadSection = styled.section`{
   }
   }
 }`
+const MyLayout = styled(Layout)`
+  display: flex;
+  flex-direction: column;
+`
 function Money(){
   return (
-    <Layout>
+    <MyLayout>
       <TagsSection>
         <ol>
           <li>衣</li>
@@ -166,7 +175,7 @@ function Money(){
           <button className="dot">.</button>
         </div>
       </NumberPadSection>
-    </Layout>
+    </MyLayout>
   )
 }
 
