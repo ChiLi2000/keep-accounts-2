b39fb64623ba679ed2bed94764b012234fdc032b
chili <1032569304@qq.com>
2020-12-15 16:35:12 +0800

    Money.tsx 之 备注 UI 完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -23,7 +23,26 @@ const TagsSection = styled.section`
   color:#666;
   margin-top:8px;
   }`
-const NotesSection = styled.section``
+const NotesSection = styled.section`
+  background: #f5f5f5;
+  padding: 0 16px;
+  font-size: 14px;
+  > label{
+  display: flex;
+  align-items: center;
+    >span{
+    margin-right: 16px;
+    white-space: nowrap;
+    }
+    >input{
+    display: block;
+    width: 100%;
+    height: 72px;
+    background: none;
+    border:none;
+    }
+}
+`
 const CategorySection = styled.section``
 const NumberPadSection = styled.section``
 function Money(){
@@ -41,7 +60,7 @@ function Money(){
       <NotesSection>
         <label>
           <span>备注</span>
-          <input type="text"/>
+          <input type="text" placeholder="在这里添加备注"/>
         </label>
       </NotesSection>
       <CategorySection>
