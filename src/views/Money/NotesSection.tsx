64fc6893175f485420c4f44b5a56a6e98e0760d9
chili <1032569304@qq.com>
2020-12-16 21:55:31 +0800

    封装 Input.tsx

--- src/views/Money/NotesSection.tsx
+++ src/views/Money/NotesSection.tsx
@@ -1,25 +1,11 @@
 import styled from "styled-components";
-import React, {useRef} from "react";
+import React, {ChangeEventHandler} from "react";
+import {Input} from "../../components/Input";
 
 const Wrapper = styled.section`
   background: #f5f5f5;
   padding: 0 16px;
   font-size: 14px;
-  > label{
-  display: flex;
-  align-items: center;
-    >span{
-    margin-right: 16px;
-    white-space: nowrap;
-    }
-    >input{
-    display: block;
-    width: 100%;
-    height: 72px;
-    background: none;
-    border:none;
-    }
-}
 `;
 
 type Props = {
@@ -28,19 +14,12 @@ type Props = {
 }
 const NotesSection: React.FC<Props> = (props) => {
   const note = props.value;
-  const refInput = useRef<HTMLInputElement>(null);
-  const onBlur = () => {
-    if (refInput.current !== null) {
-      props.onChange(refInput.current.value);
-    }
-  };
+  const onChange:ChangeEventHandler<HTMLInputElement> =(e)=>{
+    props.onChange(e.target.value)
+  }
   return (
     <Wrapper>
-      <label>
-        <span>备注</span>
-        <input type="text" placeholder="在这里添加备注"
-               ref={refInput} defaultValue={note} onBlur={onBlur}/>
-      </label>
+      <Input label="备注" type="text" value={note} onChange={onChange}/>
     </Wrapper>
   );
 };
