eb80fb41cc85fbcaed3d2f3639dcfbb99ace3bba
chili <1032569304@qq.com>
2020-12-16 12:04:31 +0800

    数字面板按钮功能

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import Layout from "../components/Layout";
 import styled from "styled-components";
 import {TagsSection} from "./Money/TagsSection";
@@ -9,34 +9,17 @@ import {NumberPadSection} from "./Money/NumberPadSection";
 const MyLayout = styled(Layout)`
   display: flex;
   flex-direction: column;
-`
-function Money(){
+`;
+
+function Money() {
   return (
     <MyLayout>
       <TagsSection/>
       <NotesSection/>
       <CategorySection/>
-      <NumberPadSection>
-        <div className="output">100</div>
-        <div className="pad clearfix">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>删除</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>清空</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button className="ok">OK</button>
-          <button className="zero">0</button>
-          <button className="dot">.</button>
-        </div>
-      </NumberPadSection>
+      <NumberPadSection/>
     </MyLayout>
-  )
+  );
 }
 
-export default Money
+export default Money;
