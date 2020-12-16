88993cb7703a5277e6e0bd87833f1ef44fb2c9a1
chili <1032569304@qq.com>
2020-12-16 22:10:43 +0800

    完成 Tag UI

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -11,7 +11,7 @@ const Label = styled.label`
     >input{
     display: block;
     width: 100%;
-    height: 72px;
+    height: 44px;
     background: none;
     border:none;
     }
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,6 +1,27 @@
 import React from "react";
-import {useTags} from "../useTags";
+import {useTags} from "useTags";
 import {useParams} from "react-router-dom";
+import styled from "styled-components";
+import Layout from "components/Layout";
+import Icon from "components/Icon";
+import {Input} from "components/Input";
+import {Center} from "components/Center";
+import {Space} from "components/Space";
+import {Button} from "../components/Button";
+
+const InputWrapper = styled.div`
+  background: white;
+  padding: 0 16px;
+  margin-top: 8px;
+`
+const Topbar = styled.header`
+  display:flex;
+  justify-content: space-between;
+  align-items: center;
+  line-height: 20px;
+  padding: 14px;
+  background:white;
+`;
 
 type Params = {
   id: string
@@ -10,7 +31,21 @@ const Tag: React.FC = () => {
   let {id} = useParams<Params>();
   const tag = findTag(parseInt(id));
   return (
-    <div>{tag.name}</div>
+    <Layout>
+      <Topbar>
+        <Icon name="left"/>
+        <span>编辑标签</span>
+      </Topbar>
+      <InputWrapper>
+        <Input label="标签名" type="text" placeholder="标签名" value={tag.name}/>
+      </InputWrapper>
+      <Center>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Button>删除标签</Button>
+      </Center>
+    </Layout>
   );
 };
 
