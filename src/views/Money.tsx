a793ce4641bd1a69fec0cec207cbedbd71b3760a
chili <1032569304@qq.com>
2020-12-16 14:51:58 +0800

    重构 money 页面数据

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -20,34 +20,19 @@ function Money() {
     category: "-" as Category,
     amount: 0
   });
+  const onChange = (obj:Partial<typeof selected>)=>{
+    setSelected({...selected,...obj})
+  }
   return (
     <MyLayout>
       <TagsSection value={selected.tags}
-                   onChange={(tags) => setSelected({
-                     ...selected,
-                     tags: tags
-                   })}/>
+                    onChange={tags=>onChange({tags})}/>
       <NotesSection value={selected.note}
-                    onChange={(note) => {
-                      setSelected({
-                        ...selected,
-                        note: note
-                      });
-                    }}/>
+                    onChange={(note) => onChange({note})}/>
       <CategorySection value={selected.category}
-                       onChange={(category) => {
-                         setSelected({
-                           ...selected,
-                           category: category
-                         });
-                       }}/>
+                       onChange={(category) => onChange({category})}/>
       <NumberPadSection value={selected.amount}
-                        onChange={(amount) => {
-                          setSelected({
-                            ...selected,
-                            amount: amount
-                          });
-                        }}
+                        onChange={(amount) => onChange({amount})}
                         onOk={() => {}}/>
     </MyLayout>
   );
