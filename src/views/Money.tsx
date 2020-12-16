25e55991327a16f0c8a779d7e84a54a0ff428f98
chili <1032569304@qq.com>
2020-12-16 10:44:59 +0800

    备注

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -14,12 +14,7 @@ function Money(){
   return (
     <MyLayout>
       <TagsSection/>
-      <NotesSection>
-        <label>
-          <span>备注</span>
-          <input type="text" placeholder="在这里添加备注"/>
-        </label>
-      </NotesSection>
+      <NotesSection/>
       <CategorySection>
         <ul>
           <li className="selected">支出</li>
--- src/views/Money/NotesSection.tsx
+++ src/views/Money/NotesSection.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import React, {useRef, useState} from "react";
 
-const NotesSection = styled.section`
+const Wrapper = styled.section`
   background: #f5f5f5;
   padding: 0 16px;
   font-size: 14px;
@@ -20,5 +21,23 @@ const NotesSection = styled.section`
     }
 }
 `
+const NotesSection:React.FC=()=>{
+  const [note,setNote]=useState('')
+  const refInput=useRef<HTMLInputElement>(null)
+  const onBlur=()=>{
+    if(refInput.current!==null){
+      setNote(refInput.current.value)
+    }
+  }
+  return(
+    <Wrapper>
+      <label>
+        <span>备注</span>
+        <input type="text" placeholder="在这里添加备注"
+        ref={refInput} defaultValue={note} onBlur={onBlur}/>
+      </label>
+    </Wrapper>
+  )
+}
 
 export {NotesSection}
