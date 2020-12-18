eb0c8b68f198c59ca7d5310e1f34699fbcadda8d
chili <1032569304@qq.com>
2020-12-18 14:26:36 +0800

    tags 数据持久化

--- src/hooks/useTags.tsx
+++ src/hooks/useTags.tsx
@@ -1,15 +1,27 @@
-import {useState} from "react";
-import {createId} from "./lib/createId";
+import {useEffect, useState} from "react";
+import {createId} from "lib/createId";
+import {useUpdate} from "./useUpdate";
 
-const defaultTags = [
-  {id: createId(), name: "衣"},
-  {id: createId(), name: "食"},
-  {id: createId(), name: "住"},
-  {id: createId(), name: "行"},
-];
 // 封装一个自定义 Hook
 const useTags = () => {
-  const [tags, setTags] = useState<{ id: number, name: string }[]>(defaultTags);
+  const [tags, setTags] = useState<{ id: number, name: string }[]>([]);
+  // 组件挂载时执行，且只执行第一次 从 0 到有
+  useEffect(() => {
+    let localTags = JSON.parse(window.localStorage.getItem("tags") || "[]");
+    if (localTags.length === 0) {
+      localTags = [
+        {id: createId(), name: "衣"},
+        {id: createId(), name: "食"},
+        {id: createId(), name: "住"},
+        {id: createId(), name: "行"},
+      ];
+    }
+    setTags(localTags);
+  }, []);
+  // 自定义 hooks 第一次不执行 变化再执行
+  useUpdate(() => {
+    window.localStorage.setItem("tags", JSON.stringify(tags));
+  }, [tags]);
   const findTag = (id: number) => tags.filter(tag => tag.id === id)[0];
   const findTagIndex = (id: number) => {
     let result = -1;
