db799b0949031725c259b1d8557eaeb1b20bc724
chili <1032569304@qq.com>
2020-12-18 15:58:53 +0800

    封装收支切换

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -11,6 +11,9 @@ const MyLayout = styled(Layout)`
   display: flex;
   flex-direction: column;
 `;
+const CategoryWrapper = styled.div`
+  background: #c4c4c4;
+`;
 
 type Category = "-" | "+"
 const defaultFormDate = {
@@ -41,8 +44,11 @@ function Money() {
                    onChange={tagIds => onChange({tagIds})}/>
       <NotesSection value={selected.note}
                     onChange={(note) => onChange({note})}/>
-      <CategorySection value={selected.category}
-                       onChange={(category) => onChange({category})}/>
+      <CategoryWrapper>
+        <CategorySection value={selected.category}
+                         onChange={(category) => onChange({category})}/>
+      </CategoryWrapper>
+
       <NumberPadSection value={selected.amount}
                         onChange={(amount) => onChange({amount})}
                         onOk={submit}/>
--- src/views/Statistics.tsx
+++ src/views/Statistics.tsx
@@ -1,11 +1,23 @@
-import Layout from "../components/Layout";
-import React from "react";
-function Statistics(){
+import Layout from "components/Layout";
+import React, {useState} from "react";
+import {CategorySection} from "./Money/CategorySection";
+import styled from "styled-components";
+
+const CategoryWrapper = styled.div`
+  background:white;
+`;
+
+function Statistics() {
+  const [category, setCategory] = useState<"-" | "+">("-");
+
   return (
     <Layout>
-      <h2>统计页面</h2>
+      <CategoryWrapper>
+        <CategorySection value={category}
+                         onChange={value => setCategory(value)}/>
+      </CategoryWrapper>
     </Layout>
-  )
+  );
 }
 
-export default  Statistics
+export default Statistics;
