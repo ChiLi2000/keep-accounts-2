ab337bb4f627f43ea8fdda96e17b1f0032a71e12
chili <1032569304@qq.com>
2020-12-18 13:36:58 +0800

    优化 tag 编辑与删除操作

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -21,23 +21,11 @@ const useTags = () => {
     }
     return result;
   };
-  const updateTag = (id: number, obj: { name: string }) => {
-    //  获取 tag 下标
-    const index = findTagIndex(id);
-    //  深拷贝 tags 得到 tagsClone
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    //  将 tagsClone 的第 index 删掉，换成 {id:id, name: obj.name}
-    tagsClone.splice(index, 1, {id: id, name: obj.name});
-    setTags(tagsClone);
+  const updateTag = (id: number, {name}: { name: string }) => {
+    setTags(tags.map(tag => tag.id ? {id, name: name} : tag));
   };
   const deleteTag = (id: number) => {
-    //  获取要删除的 tag 下标
-    const index = findTagIndex(id);
-    //  深拷贝 tags 得到 tagsClone
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    //  把 tagClone 的第 index 删掉
-    tagsClone.splice(index, 1);
-    setTags(tagsClone);
+    setTags(tags.filter(tag => tag.id !== id));
   };
   return {tags, setTags, findTag, findTagIndex, updateTag, deleteTag};
 };
